2b7eafbe823f17775c5574f17c00dd7c3c6c38da
Krystan Menne <kmenne@gmail.com>
2016-06-19 15:39:20 -0700

    show a message on the index when there are no posts

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from .models import Post
 from .factories import PostFactory
+from .models import Post
 
 
 class PostMethodTests(TestCase):
@@ -11,3 +12,13 @@ class PostMethodTests(TestCase):
         """
         post = PostFactory()
         self.assertEqual(Post.objects.last(), post)
+
+
+class PostViewTests(TestCase):
+    def test_index_view_with_no_posts(self):
+        """
+        If no posts exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('blog:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no posts.')
