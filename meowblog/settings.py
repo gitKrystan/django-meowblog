f37a72ea51b9a19078a27c8260c7da4a1643c449
Krystan Menne <kmenne@gmail.com>
2016-06-22 09:34:34 -0700

    add staticfiles finders to fix admin css

--- meowblog/settings.py
+++ meowblog/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_FINDERS = (
     # other static files finder
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
 
